5b1d92802b893f10c3b311b5cd813f3f4c875a8a
SANDOR FERREIRA DA SILVA <sandorferreira@gmail.com>
2017-06-15 11:07:24 -0300

    inicio leitura

--- src/Programa.java
+++ src/Programa.java
@@ -1,4 +1,23 @@
+import java.io.File;
 
 public class Programa {
-
+	public static void main(String[] args) {
+	
+	}
+	
+	public static void abrirArquivosDeEntrada() {
+		File fileDocente = new File("docentes.csv");
+		if (fileDocente.canRead()) {
+			// criar objetos docente e ir ao proximo
+		}
+		
+		File filePublicacoes = new File("publicacoes.csv");
+		
+		File fileQualis = new File("qualis.csv");
+		
+		File fileRegras = new File("regras.csv");
+		
+		File fileVeiculos = new File("veiculos.csv");
+	}
+	
 }
